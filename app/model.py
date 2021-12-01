b6ca9c0bf572224dacad01a4e0f2082aacb4690b
Pierre Virgaux <pierre.virgaux@eisti.fr>
2021-12-01 15:21:02 +0100

    Model final

--- app/model.py
+++ app/model.py
@@ -5,13 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-data = pd.read_csv('../data/EuroMillions_numbers.csv', sep = ";", parse_dates = True)
-data = data.drop(['Winner',	'Gain', 'Date'], axis = 1)
-data['Winner'] = 1
-
+chemin_fichier = "../data/EuroMillions_numbers.csv"
 
 def generation_data_perdante(nb):
-    
+    '''
+    Permet la génération de donnée perdante au loto pour un future entraînement.Le tirage est aléatoire et sans remise
+
+    nb: Nombre de lignes à générer 
+    '''    
     ligne_data = []
     for i in range(nb):
 
@@ -23,13 +24,24 @@ def generation_data_perdante(nb):
     df['Winner'] = 0
     return df
 
-data_perdu = generation_data_perdante(8 * data.shape[0])
-data_complete = pd.concat([data, data_perdu])
-X = data_complete.drop('Winner', axis = 1)
-y = data_complete['Winner']
+
+def creation_data():
+    "Permet la création du tableau de données"
+
+    data = pd.read_csv(chemin_fichier, sep = ";")
+    data = data.drop(['Winner',	'Gain', 'Date'], axis = 1)
+    data['Winner'] = 1
+
+    data_perdu = generation_data_perdante(8 * data.shape[0])
+    data_complete = pd.concat([data, data_perdu])
+    X = data_complete.drop('Winner', axis = 1)
+    y = data_complete['Winner']
+
+    return (X, y)
 
 
 def entrainement(X,y):
+    X,y = creation_data(chemin_fichier)
     X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle = True, stratify=y)
     foret = RandomForestClassifier(oob_score=True).fit(X_train, y_train)
     return foret
